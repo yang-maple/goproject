3370d6663408b1b7e58f5bb8d84f8d162c8bf0c1
499968985@qq.com <499968985@qq.com>
2025-01-15 20:06:05 +0800

    2025/1/15

--- tools/times/main.go
+++ tools/times/main.go
@@ -11,6 +11,8 @@ func main() {
 	fmt.Println("当前时间：", now)
 	fmt.Println("减去 8 小时后的时间：", adjustedTime)
 	adjustedTime2 := time.Now().Add(time.Hour * -9).Format("2006-01-02T15:04:05Z")
-	fmt.Println("减去 8 小时后的时间：", adjustedTime2)
+	fmt.Println("减去 9 小时后的时间：", adjustedTime2)
+	adjustedTime3 := time.Now().Add(time.Hour * -10).Format("2006-01-02T15:04:05Z")
+	fmt.Println("减去 10 小时后的时间：", adjustedTime3)
 
 }
